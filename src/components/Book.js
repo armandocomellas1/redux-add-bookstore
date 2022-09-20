341e1aac47ddfc2d3becb9c3840158e0dd41ab43
GameMaster <businesswitharmando@gmail.com>
2022-09-20 15:04:05 -0500

    Fixed linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,13 +3,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
-import { delBook } from '../redux/books/books_duck';
 
 const Book = (props) => {
   const dispatch = useDispatch();
   const submit = (event) => {
     const getIDtoDel = event.target.parentElement.parentElement.id;
-    console.log('getIDtoDel', getIDtoDel);
     document.getElementById(getIDtoDel).remove();
     // dispatch(delBook(event));
   };
